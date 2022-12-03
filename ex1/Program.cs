1271c3e4374a216cf06253e1a735bc9a84b1467c
Hkate <hkate1985@gmail.com>
2022-12-03 13:19:25 +0300

    Task1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,1 +1,13 @@
 ﻿Console.WriteLine("введите два числа:");
+int numberA=Convert.ToInt32(Console.ReadLine());
+int numberB=Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.Write("Max:");
+    Console.Write(numberA);
+}
+else
+{
+    Console.Write("Max:");
+    Console.Write(numberB);
+}
