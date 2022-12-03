2613ebe73802e9a126e580cf099e5e035cec6b18
Hkate <hkate1985@gmail.com>
2022-12-03 14:02:20 +0300

    Task3

---
+++ ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int numberA=Convert.ToInt32(Console.ReadLine());
+if (numberA%2==0)
+{
+    Console.Write(numberA);
+    Console.Write("-четное число");
+}
+else
+{
+    Console.Write(numberA);
+    Console.Write("-нечетное число");
+}
