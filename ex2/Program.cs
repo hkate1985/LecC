39c0f0f60f886f1ffc526cbb5bddcef36bb518a0
Hkate <hkate1985@gmail.com>
2022-12-03 13:50:40 +0300

    Task2

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hell2222o, World!");
+﻿Console.WriteLine("введите три числа:");
+int numberA=Convert.ToInt32(Console.ReadLine());
+int numberB=Convert.ToInt32(Console.ReadLine());
+int numberC=Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    if (numberA>numberC)
+    {
+        Console.Write("Max:");
+        Console.Write(numberA);
+    }
+    else
+    {
+        if (numberC>numberB)
+        {
+            Console.Write("Max:");
+            Console.Write(numberC);
+        }
+        else
+        {
+            Console.Write("Max:");
+            Console.Write(numberB);
+        }
+    }
+}
+else
+{
+    if (numberB>numberC)
+    {
+        Console.Write("Max:");
+        Console.Write(numberB);
+    }
+    else
+    {
+        Console.Write("Max:");
+        Console.Write(numberC);
+    }
+}
