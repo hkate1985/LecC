2ea502fdf844584eeefbbb585ec974055182b3ff
Hkate <hkate1985@gmail.com>
2022-12-05 12:31:56 +0300

    Task3

---
+++ 2Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Task3.Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("(выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("это  не день недели");
+  }
+  else Console.WriteLine("(рабочий день) -> да");
+
