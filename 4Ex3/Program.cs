050da5e21d10187930953e04d450b7f8413db23e
Hkate <hkate1985@gmail.com>
2022-12-08 17:04:30 +0300

    Task3

---
+++ 4Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Task 3.Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// (int size = 8);
+int[] array = new int[8];
+Random ARandom = new Random();
+Console.WriteLine("Вывод массива");
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ARandom.Next(0,8);
+        Console.Write("{0} ", array[i]);
+    }
+ 
+
