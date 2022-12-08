515244941371b0d6b2a40e39b9c2f0db3e2b7d78
Hkate <hkate1985@gmail.com>
2022-12-08 13:49:01 +0300

    Task2

---
+++ 4Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Task2.Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int count=0;
+if (a<0)
+{
+    a=-a;
+}
+    while  (a>0)     
+    {
+        int num = a % 10;
+        a = a / 10;
+        sum = sum + num;
+        count++;
+    }
+Console.WriteLine(sum);
+   
