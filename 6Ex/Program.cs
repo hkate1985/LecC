8a985e8ff67188449257af3b7da110ad36801826
Hkate <hkate1985@gmail.com>
2022-12-13 17:50:39 +0300

    Tasks

---
+++ 6Ex/Program.cs
@@ -0,0 +1,39 @@
+﻿
+switch (2)
+{
+    case 1:
+        Console.WriteLine();
+        break;
+    case 2:
+        Console.WriteLine();
+        break;
+    default:
+       break; 
+}
+Console.WriteLine("Task1.Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
+
+Console.WriteLine("Task2.Напишите программу, которая найдёт точку пересечения двух прямых заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.");
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+
+
